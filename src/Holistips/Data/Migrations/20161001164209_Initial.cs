6f95b84c04fe60013d214759692fad11eaa762b2
event <event@DESKTOP-MF9ARKU>
2016-10-11 18:30:18 +0300

    Add project files.

---
+++ src/Holistips/Data/Migrations/20161001164209_Initial.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Holistips.Data.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tips",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    TipAnalogy = table.Column<string>(nullable: true),
+                    TipExplanation = table.Column<string>(nullable: true),
+                    TipHashtags = table.Column<string>(nullable: true),
+                    TipRefs = table.Column<string>(nullable: true),
+                    TipTitle = table.Column<string>(nullable: true),
+                    TipWhenTo = table.Column<string>(nullable: true),
+                    TipWhere = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tips", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tips");
+        }
+    }
+}
---
+++ src/Holistips/Models/Tip.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Holistips.Models
+{
+    public class Tip
+    {
+        public int ID { get; set; }
+
+        public string TipTitle { get; set; }
+
+        public string TipExplanation { get; set; }
+
+        public string TipWhenTo { get; set; }
+
+        public string TipWhere { get; set; }
+
+        public string TipAnalogy { get; set; }
+
+        public string TipRefs { get; set; }
+
+        public string TipHashtags { get; set; }
+    }
+}
